a99358c29719def0bffc42de92a6523a7ed793c7
SardorUrokov <urokovsardor70@gmail.com>
2023-06-23 18:05:40 +0500

    Changes

--- src/main/java/com/example/pdp_esm/component/Dataloader.java
+++ src/main/java/com/example/pdp_esm/component/Dataloader.java
@@ -24,17 +24,16 @@ import static com.example.pdp_esm.entity.enums.Roles.*;
 @RequiredArgsConstructor
 public class Dataloader implements CommandLineRunner {
 
-    private final CourseRepository courseRepository;
-    private final TeacherRepository teacherRepository;
     private final GroupRepository groupRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AnswerRepository answerRepository;
+    private final CourseRepository courseRepository;
     private final StudentRepository studentRepository;
+    private final TeacherRepository teacherRepository;
     private final PaymentRepository paymentRepository;
     private final QuestionRepository questionRepository;
     private final PositionRepository positionRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final ExamResultRepository resultRepository;
     private final AuthenticationService authenticationService;
-    private final AnswerRepository answerRepository;
 
     @Value("${spring.sql.init.mode}")
     private String mode;
@@ -63,37 +62,50 @@ public class Dataloader implements CommandLineRunner {
             Student a1_student = studentRepository.save(new Student("Usmon Saidiy", "998914525468", "urokovsardor15@gmail.com", passwordEncoder.encode("password123"), "Male", USER, true, 1100000d, Status.WAITING, A1));
             Student a1_student1 = studentRepository.save(new Student("Anvar Anvarov", "998914525468", "urokovsardorgpt@gmail.com", passwordEncoder.encode("password123"), "Male", USER, true, 1100000d, Status.WAITING, A1));
             Student f1_student = studentRepository.save(new Student("MuhammadAziz Zayniddinov", "998914525468", "sardorurokov92@gmail.com", passwordEncoder.encode("password123"), "Male", USER, true, 1000000d, Status.COMPLETED, F1));
-            
-            Question question1 = questionRepository.save(new Question("There's a 1st question", QuestionType.TRUE_FALSE, true, android_course));
-            Question question2 = questionRepository.save(new Question("There's a 2nd question", QuestionType.TRUE_FALSE, true, android_course));
+
+            Question question1 = questionRepository.save(new Question("There's a 1st question", QuestionType.TRUE_FALSE, true, java_backend_course));
+            Question question2 = questionRepository.save(new Question("There's a 2nd question", QuestionType.TRUE_FALSE, true, java_backend_course));
             Question question3 = questionRepository.save(new Question("There's a 3rd question", QuestionType.TEST, true, java_backend_course));
             Question question4 = questionRepository.save(new Question("There's a 4th question", QuestionType.TEST, true, java_backend_course));
-            Question question5 = questionRepository.save(new Question("There's a 5th question", QuestionType.SEQUENCE, true, frontEnd_course));
-            Question question6 = questionRepository.save(new Question("There's a 6th question", QuestionType.SEQUENCE, true, frontEnd_course));
+            Question question5 = questionRepository.save(new Question("There's a 5th question", QuestionType.SEQUENCE, true, java_backend_course));
+            Question question6 = questionRepository.save(new Question("There's a 6th question", QuestionType.SEQUENCE, true, java_backend_course));
             Question question7 = questionRepository.save(new Question("There's a 7th question", QuestionType.WRITE_MISSING_WORD, true, java_backend_course));
 
-            Answer answerTest = answerRepository.save(new Answer("TRUE", true, 1, question1));
-            Answer answerTest1 = answerRepository.save(new Answer("FALSE", false, 2, question1));
- 
-            Answer answerTest2 = answerRepository.save(new Answer("bbb", false, 1, question3));
-            Answer answerTest3 = answerRepository.save(new Answer("aaa", true, 2, question3));
-            Answer answerTest4 = answerRepository.save(new Answer("ddd", false, 3, question3));
-            Answer answerTest5 = answerRepository.save(new Answer("ssss", false, 4, question3));
+            answerRepository.save(new Answer("TRUE", true, 1, question1));
+            answerRepository.save(new Answer("FALSE", false, 2, question1));
+            answerRepository.save(new Answer("FALSE", true, 1, question2));
+            answerRepository.save(new Answer("TRUE", false, 2, question2));
 
-            Answer answerTest6 = answerRepository.save(new Answer("a1", true, 1, question5));
-            Answer answerTest7 = answerRepository.save(new Answer("a2", true, 2, question5));
-            Answer answerTest8 = answerRepository.save(new Answer("a3", true, 3, question5));
+            answerRepository.save(new Answer("bbb", false, 1, question3));
+            answerRepository.save(new Answer("aaa", true, 2, question3));
+            answerRepository.save(new Answer("ddd", false, 3, question3));
+            answerRepository.save(new Answer("ssss", false, 4, question3));
 
-            Answer answerTest9 = answerRepository.save(new Answer("there's a missing word", true, 1, question7));
+            answerRepository.save(new Answer("alt", false, 1, question4));
+            answerRepository.save(new Answer("shift", false, 2, question4));
+            answerRepository.save(new Answer("ctrl", false, 3, question4));
+            answerRepository.save(new Answer("qwerty", true, 4, question4));
+
+            answerRepository.save(new Answer("a1", true, 1, question5));
+            answerRepository.save(new Answer("a2", true, 2, question5));
+            answerRepository.save(new Answer("a3", true, 3, question5));
+
+            answerRepository.save(new Answer("alt1", true, 1, question5));
+            answerRepository.save(new Answer("alt2", true, 2, question5));
+            answerRepository.save(new Answer("alt3", true, 3, question5));
+            answerRepository.save(new Answer("alt4", true, 4, question5));
+            answerRepository.save(new Answer("alt5", true, 5, question5));
+
+            answerRepository.save(new Answer("there's a missing word", true, 1, question7));
+
+            paymentRepository.save(new Payment(1100000d, a1_student, PayType.CASH, PayStatus.RECEIVED));
+            paymentRepository.save(new Payment(1000000d, f1_student, PayType.BY_PAYMENT_APP, PayStatus.RECEIVED));
+            paymentRepository.save(new Payment(1200000d, j1_student, PayType.BY_CARD, PayStatus.RECEIVED));
 
 //            ExamResult examResult1 = resultRepository.save(new ExamResult(80f, f1_student, ResultType.SUCCESS, List.of(question5, question6)));
 //            ExamResult examResult2 = resultRepository.save(new ExamResult(60f, a1_student1, ResultType.SUCCESS, List.of(question1, question2)));
 //            ExamResult examResult3 = resultRepository.save(new ExamResult(50f, a1_student, ResultType.FAIL, List.of(question2, question1)));
 //            ExamResult examResult4 = resultRepository.save(new ExamResult(70f, j1_student, ResultType.SUCCESS, List.of(question3, question4)));
-
-            Payment payment1 = paymentRepository.save(new Payment(1100000d, a1_student, PayType.CASH, PayStatus.RECEIVED));
-            Payment payment2 = paymentRepository.save(new Payment(1000000d, f1_student, PayType.BY_PAYMENT_APP, PayStatus.RECEIVED));
-            Payment payment3 = paymentRepository.save(new Payment(1200000d, j1_student, PayType.BY_CARD, PayStatus.RECEIVED));
 
             var aaa = RegisterRequest.builder()
                     .fullName("AAA")
@@ -102,7 +114,6 @@ public class Dataloader implements CommandLineRunner {
                     .role(USER)
                     .build();
             System.out.println("AAA user token: " + authenticationService.register(aaa).getAccessToken());
-
         }
     }
 }
